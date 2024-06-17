14ca9f6caf972e7df77c8966654a1cb3df58a1d6
Mote Kumar <motekumarm5@gamil.com>
2024-06-17 12:32:41 +0530

    added config changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { google } = require("googleapis");
 const cors = require("cors");
 const bodyParser = require("body-parser")
-import credentials from "./config.js";
+const credentials  = required("./config.js") ;
 
 //initilize express
 const app = express();
