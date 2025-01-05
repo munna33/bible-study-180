e22cec2d35717debbd1bc29c30523b8e1aa4bde8
Mote Kumar <29727975+munna33@users.noreply.github.com>
2025-01-05 23:12:14 +0530

    env config changes

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 const credentials = {
- "type": "service_account",
- "project_id": process.env.PROJECT_ID,
+  "type": "service_account",
+  "project_id": process.env.PROJECT_ID,
   "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
+  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace \n with actual newline
   "client_email": process.env.CLIENT_EMAIL,
   "client_id": process.env.CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bible-study%40bible-study-446706.iam.gserviceaccount.com",
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
   "universe_domain": "googleapis.com"
-}
+};
+
 module.exports = credentials;
--- index.js
+++ index.js
@@ -377,6 +377,15 @@ let projectId = process.env.PROJECT_ID || 'bible-study-446706';
 app.listen(port, () => {
   console.log("server is running on", port);
   console.log("app name:", projectId);
+  console.log({
+    PROJECT_ID: process.env.PROJECT_ID,
+    PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
+    PRIVATE_KEY: process.env.PRIVATE_KEY ? 'Exists' : 'Missing',
+    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_X509_CERT_URL: process.env.CLIENT_X509_CERT_URL
+  });
+  
 });
 
 app.post('/getScoreByID/:id/', async(req, res) => {
