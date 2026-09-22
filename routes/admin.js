a587a2975896505cc55323818914a0b2fbb75289
Mote Kumar <29727975+munna33@users.noreply.github.com>
2026-09-22 14:58:38 +0530

    added program details with dynamic handle changes

--- routes/admin.js
+++ routes/admin.js
@@ -714,6 +714,7 @@ router.post("/saveDailyQuizData", async (req, res) => {
       }
     }
     let result = dailyQuizData;
+    
     // if (!dailyQuizData || dailyQuizData.length === 0) {
     //   return res.status(404).send({ error: "No puzzle scores found" });
     // }
